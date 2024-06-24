693c38637d7e07197b5a649a82359ae11bea876c
Romario Marcal <romariomarcal11@gmail.com>
2024-06-25 02:13:47 +0900

    update the content

--- .dart_tool/flutter_build/4e6866059e50a3bbc54a12c2a7bcf575/web_plugin_registrant.dart
+++ .dart_tool/flutter_build/4e6866059e50a3bbc54a12c2a7bcf575/web_plugin_registrant.dart
@@ -10,7 +10,10 @@ import 'package:audio_session/audio_session_web.dart';
 import 'package:audioplayers_web/audioplayers_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:just_audio_web/just_audio_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_plus/src/wakelock_plus_web_plugin.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -19,6 +22,9 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   AudioplayersPlugin.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   JustAudioPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WakelockPlusWebPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
